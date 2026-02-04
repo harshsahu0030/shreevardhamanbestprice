6f721767b17f627874330e5d045506f568838baa
harshsahu0030 <harsh0030sahu@gmail.com>
2026-02-04 20:22:17 +0530

    add-index-hero

--- js/header.js
+++ js/header.js
@@ -1,7 +1,38 @@
+const headerLinks = [
+  {
+    label: "Home",
+    url: "/index.html",
+  },
+  {
+    label: "About Us",
+    url: "/",
+  },
+  {
+    label: "Categories",
+    url: "/",
+  },
+  {
+    label: "Contact",
+    url: "/",
+  },
+];
+
 document.addEventListener("DOMContentLoaded", (event) => {
   const btnOpen = document.querySelector(".nav-open");
   const btnClose = document.querySelector(".nav-close");
   const navbar = document.getElementById("website-navbar");
+  const links = document.querySelector("#nav-links");
+
+  headerLinks.forEach((link) => {
+    links.innerHTML += `<li class="hover:scale-95 transition-all group w-fit uppercase">
+              <a href="${link?.url}"
+                >${link?.label}
+                <span
+                  class="opacity-0 group-hover:opacity-100 transition hidden xl:inline-block"
+                  >-></span
+                ></a
+              >`;
+  });
 
   let navTimeline = gsap.timeline({
     paused: true,
